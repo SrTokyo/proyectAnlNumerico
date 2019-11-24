344e7d017d8197605e480810554d0890a18f1e15
SrTokyo <52584498+SrTokyo@users.noreply.github.com>
2019-11-24 10:33:29 -0500

    Menu created

--- build-Gui_OCanada-Desktop_Qt_5_12_6_MSVC2017_32bit-Debug/ui_mainwindow.h
+++ build-Gui_OCanada-Desktop_Qt_5_12_6_MSVC2017_32bit-Debug/ui_mainwindow.h
@@ -11,9 +11,10 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,8 +23,16 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QMenuBar *menubar;
-    QStatusBar *statusbar;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QLabel *label;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -32,13 +41,52 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QString::fromUtf8("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(20, 120, 751, 321));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton = new QPushButton(verticalLayoutWidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(verticalLayoutWidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(verticalLayoutWidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        verticalLayout->addWidget(pushButton_3);
+
+        pushButton_4 = new QPushButton(verticalLayoutWidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+
+        verticalLayout->addWidget(pushButton_4);
+
+        pushButton_5 = new QPushButton(verticalLayoutWidget);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+
+        verticalLayout->addWidget(pushButton_5);
+
+        pushButton_6 = new QPushButton(verticalLayoutWidget);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+
+        verticalLayout->addWidget(pushButton_6);
+
+        pushButton_7 = new QPushButton(verticalLayoutWidget);
+        pushButton_7->setObjectName(QString::fromUtf8("pushButton_7"));
+
+        verticalLayout->addWidget(pushButton_7);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(220, 20, 381, 81));
+        label->setStyleSheet(QString::fromUtf8("font: 16pt \"Elephant\";"));
         MainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
-        MainWindow->setMenuBar(menubar);
-        statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName(QString::fromUtf8("statusbar"));
-        MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
 
@@ -47,7 +95,16 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "O'Canada Proyecto Analisis Numerico", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "Busqueda incremental", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Metodos cerrados", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Metodos abiertos", nullptr));
+        pushButton_4->setText(QApplication::translate("MainWindow", "Eliminacion Gaussiana", nullptr));
+        pushButton_5->setText(QApplication::translate("MainWindow", "Metodos de factorizaci\303\263n", nullptr));
+        pushButton_6->setText(QApplication::translate("MainWindow", "Metodos Iterativos", nullptr));
+        pushButton_7->setText(QApplication::translate("MainWindow", "Metodos Interpolacion", nullptr));
+        label->setText(QApplication::translate("MainWindow", "                     \302\241\302\241 Bienvenidos !!\n"
+"       Selecione un metodo por favor.", nullptr));
     } // retranslateUi
 
 };
