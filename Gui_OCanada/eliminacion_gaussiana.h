634abcc552f250c92ad9f5aeaf28c12059ca08c1
SrTokyo <52584498+SrTokyo@users.noreply.github.com>
2019-11-25 11:22:26 -0500

    Add Pivote_Parcial

--- Gui_OCanada/eliminacion_gaussiana.h
+++ Gui_OCanada/eliminacion_gaussiana.h
@@ -4,6 +4,7 @@
 #include <QFrame>
 #include <QInputDialog>
 #include "simple.h"
+#include "pivote_parcial.h"
 
 namespace Ui {
 class Eliminacion_Gaussiana;
---
+++ Gui_OCanada/pivote_parcial.h
@@ -0,0 +1,51 @@
+#ifndef PIVOTE_PARCIAL_H
+#define PIVOTE_PARCIAL_H
+
+#include <QWidget>
+#include <cstring>
+#include <QTableWidget>
+#include <QString>
+#include <QLabel>
+#include <QLineEdit>
+#include <QInputDialog>
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#define DECIMALS 13
+
+namespace Ui {
+class Pivote_Parcial;
+}
+
+class Pivote_Parcial : public QWidget
+{
+    Q_OBJECT
+
+public:
+    int n;
+    double** matriz;
+    double* vectorB;
+    double* solvX;
+    double* mult;
+    double** mAum;
+    void escribir_solucion();
+    void resolver_sistema();
+    void extraer_matriz();
+    void init(int m);
+    void solve();
+    void imp_etapa();
+    int etapa_counter;
+    int vez;
+    explicit Pivote_Parcial(QWidget *parent = nullptr);
+    ~Pivote_Parcial();
+
+private slots:
+    void on_bt_setAb_clicked();
+
+    void on_solve_clicked();
+
+private:
+    Ui::Pivote_Parcial *ui;
+};
+
+#endif // PIVOTE_PARCIAL_H
